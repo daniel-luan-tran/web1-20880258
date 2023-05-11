5df8d7a86b50e169cbb50db7343f7babd6c4993c
luan.tran <luan.tran@blackbook.ai>
2023-05-11 19:45:52 +0700

    Update: Blogs

--- js/20880258.js
+++ js/20880258.js
@@ -1,4 +1,5 @@
 const apiRootUrl = 'https://web1-api.vercel.app/api';
+
 async function loadData(request, templateId, viewId) {
     const res = await fetch(`${apiRootUrl}/${request}`);
     
@@ -7,6 +8,24 @@ async function loadData(request, templateId, viewId) {
     var template = Handlebars.compile(source);
 
     var context = { [`${viewId.replace("-", "")}`]: data };
+    var productsView = document.getElementById(`${viewId}`);
+    console.log(template(context))
+    productsView.innerHTML = template(context);
+};
+
+async function loadBlogs(request, templateId, viewId, currentPage = 1) {
+    const res = await fetch(`${apiRootUrl}/${request}?page=${currentPage}`);
+    const data = await res.json();
+    var source = document.getElementById(`${templateId}`).innerHTML;
+    var template = Handlebars.compile(source);
+
+    var context = { 
+        [`${viewId.replace("-", "")}`]: data.data, 
+        currentPage: currentPage, 
+        request: request, 
+        pageCount: data.pageCount 
+    };
+    
     var productsView = document.getElementById(`${viewId}`);
     console.log(template(context))
     productsView.innerHTML = template(context);
