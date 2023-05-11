5eca7321dce9606c17222a67eaf8daeb0a0b07be
luan.tran <luan.tran@blackbook.ai>
2023-05-11 23:48:54 +0700

    Update: login and logout

--- js/20880258.js
+++ js/20880258.js
@@ -1,5 +1,7 @@
 const apiRootUrl = 'https://web1-api.vercel.app/api';
-
+const AUTHENTICATIE_API = 'https://web1-api.vercel.app/users';
+//username: web1
+//password: W3b1@Project
 async function loadData(request, templateId, viewId) {
     const res = await fetch(`${apiRootUrl}/${request}`);
     
@@ -30,3 +32,77 @@ async function loadBlogs(request, templateId, viewId, currentPage = 1) {
     console.log(template(context))
     productsView.innerHTML = template(context);
 };
+
+async function getAuthToken(username, password) {
+    let res = await fetch(`${AUTHENTICATIE_API}/authenticate`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    });
+
+    let result = await res.json();
+    if (res.status == 200) {
+        return result.token;
+    }
+    throw new Error(result.message);
+}
+
+async function login(e) {
+    e.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    try {
+        let token = await getAuthToken(username, password);
+        if (token) {
+            localStorage.setItem('token', token);
+            document.getElementsByClassName('btn-close')[0].click();
+            displayControls();
+        }
+    } catch (error) {
+        document.getElementById('errorMessage').innerHTML = error;
+        displayControls(false);
+    }
+}
+
+async function logout() {
+    localStorage.removeItem('token');
+    checkLogin();
+}
+
+function displayControls(isLogin = true) {
+    let linkLogins = document.getElementsByClassName('linklogin');
+    let linklogouts = document.getElementsByClassName('linklogout');
+
+    let displayLogin = 'none';
+    let displayLogout = 'block';
+
+    if (!isLogin) {
+        displayLogin = 'block';
+        displayLogout = 'none';
+    }
+
+    for (let i = 0; i < 2; i++) {
+        linkLogins[i].style.display = displayLogin;
+        linklogouts[i].style.display = displayLogout;    
+    }
+}
+
+async function checkLogin() {
+    let token = localStorage.getItem('token');
+    if (!token) {
+        displayControls(false);
+        return;
+    } else {
+        displayControls();
+    }
+}
+
+checkLogin();
+logout();
